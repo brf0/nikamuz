b84fb347d61d99dfe1ad46200034ec660acea73f
bahar farshchian <farshchianb@gmail.com>
2025-01-24 11:20:00 +0330

    Add files via upload

---
+++ AutoScanForDependencyAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace AttributeSamples.CustomAttributes;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+// به این صورت می تونیم بگیم که اتربیتمون فقط توی سطح اسمبلی قابل استفاده باشه نه جای دیگه ای
+public class AutoScanForDependencyAttribute : Attribute
+{
+}
+[AttributeUsage(AttributeTargets.Class| AttributeTargets.Interface| AttributeTargets.Struct | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+// در اینجا اومدیم و گفتیم که این اتربیتمون توی کلاس ها اینترفیس ها و... قابل دسترسی باشه و همچنین با استفاده از
+// AllowMultiple = true
+// گفتیم که می تونیم از این اتربیوت چندبار برای یک تایپی استفاده کنیم و با
+// Inherited = false
+// هم گفتیم که نمیشه ازش ارث بری کرد
+public class CodeChangeHistoryAttribute : Attribute
+{
+    private readonly string _author;
+
+    public string Description { get; set; }
+    public DateTime ChangeDateTime { get; set; }
+    public bool IsBug { get; }
+
+    public CodeChangeHistoryAttribute(string author, bool isBug = false)
+    {
+        _author = author;
+        IsBug = isBug;
+    }
+    // در اینجا هم اومدیم و برای اتربیتمون یکسری پراپرتی و یک کانستراکتور تعریف کردیم
+}
---
+++ Person.cs
@@ -0,0 +1,63 @@
+﻿using AttributeSample.Domain;
+using AttributeSamples.CustomAttributes;
+
+//using AttributesSamples.CustomAttributes;
+using System.ComponentModel.DataAnnotations;
+// با استفاده از این یوزینگ می تونیم از اتربیوت ها برای اعتبارسنجی داده هامون استفاده کنیم.
+using System.Diagnostics;
+// سه
+// attributes
+// زیر داخل این یوزینگ قرار دارند
+// [DebuggerDisplay]
+// توی این اتربیوت می تونیم بگیم که چه اطلاعاتی می خوایم نمایش داده بشه
+// [DebuggerBrowsableState]
+// توی این اتربیوت می تونیم تعیین کنیم که کدوم یکی از ویژگی هایی که تایپ ما هستند قابل دیدن باشند
+// [DebuggerTypeProxy]
+// و توی این اتربیوت می تونیم یک پراکسی بنویسیم برای این که یک تایپ خاصی رو بتونیم نمایش بدیم
+
+//namespace AttributeSample.Domain;
+
+[DebuggerDisplay("Person Name is {FirstName} and his/her age is {Age}")]
+// اطلاعاتی که اینجا نوشتیم رو تو خود برنامه در زمان دیباگ می تونیم ببینیم
+
+[DebuggerTypeProxy(typeof(PersonDebuggerTypeProxy))]
+// به این اتربیوت باید یک تایپ بدیم و اون تایپمون هم حتما باید کانستراکتور داشته باشه
+// و درآخر هم کاری که این اتربیوت می کنه اینه که میاد و دوتا پراپرتی ای که داخل این کلاس تعریف کردیم رو در زمان دیباگ نمایش میده
+
+[Serializable]
+// با استفاده از این اتربیوت می تونیم تایپمون (کلاسی که در زیر نوشتیم) رو سریالایز کنیم
+
+[CodeChangeHistory("Bahar Farshchian", isBug: false, Description = "Add New Property with Name ...")]
+[CodeChangeHistory("Bahar Farshchian", isBug: true, Description = "Fix the bug")]
+// به این صورت می تونیم از اتربیوتی که ساختیم استفاده کنیم و اطلاعاتی می خوایم رو توش بنویسیم
+public class Person
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    // اینجوی دیگه پراپرتی ای که در زیر نوشتیم در قسمت اتربیوت ها توی
+    // program
+    // و زمان دیباگ نمایش داده نمیشه
+    
+    [Required]
+    public string FirstName { get; set; } = "bahar";
+    [StringLength(100)]
+    // اینجا به این صورت میگیم که حداکثر طول رشتمون 100 باشه
+
+    [CodeChangeHistory("Bahar Farshchian", isBug: false, Description = "Use Attribute in code for property")]
+    public string LastName { get; set; } = "farshchian";
+
+    public int Age { get; set; }
+
+    [NonSerialized]
+    // و به این صورت هم می تونیم جلوی سریالایز شدن یک تایپ (که در اینجا فیلد هست) رو بگیریم
+    private int _age;
+
+    public void Print()
+    {
+        Console.WriteLine($"{FirstName} {LastName}");
+    }
+
+    public void InputPrinter(string myinput)
+    {
+        Console.WriteLine(myinput);
+    }
+}
